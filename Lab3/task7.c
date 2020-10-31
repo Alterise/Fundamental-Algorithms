3b0fc31028b4b207b93d3632959b5abf0063fd68
Alex <alex.tereshkov01@mail.ru>
2020-10-31 14:34:22 +0300

    Update task7.c

--- Lab3/task7.c
+++ Lab3/task7.c
@@ -5,14 +5,14 @@
 #include <math.h>
 #include <stdarg.h>
 
-typedef struct Time
+typedef struct 
 {
     int hours;
     int minutes;
     int seconds;
 } Time;
 
-typedef struct Flight
+typedef struct
 {
     int num;
     char *dep_airport;
@@ -23,6 +23,22 @@ typedef struct Flight
     Time arr_time;
 } Flight;
 
+shrink(Flight *list, int to_del, int *count)
+{
+    --*count;
+    list[to_del].num = list[*count].num;
+    strcpy(list[to_del].dep_airport, list[*count].dep_airport);
+    strcpy(list[to_del].dep_city, list[*count].dep_city);
+    strcpy(list[to_del].arr_airport, list[*count].arr_airport);
+    strcpy(list[to_del].arr_city, list[*count].arr_city);
+    list[to_del].dep_time.hours = list[*count].dep_time.hours;
+    list[to_del].dep_time.minutes = list[*count].dep_time.minutes;
+    list[to_del].dep_time.seconds = list[*count].dep_time.seconds;
+    list[to_del].arr_time.hours = list[*count].arr_time.hours;
+    list[to_del].arr_time.minutes = list[*count].arr_time.minutes;
+    list[to_del].arr_time.seconds = list[*count].arr_time.seconds;
+    list = (Flight*)realloc(list, *count * sizeof(Flight));
+}
 
 int main(int argc, char **argv)
 {
@@ -46,14 +62,54 @@ int main(int argc, char **argv)
     while (!feof(fin))
     {
         fscanf(fin, "%d %s %s %s %s ", &num, dep_airport, dep_city, arr_airport, arr_city);
-        fscanf(fin, "%d:%d:%d ", dep_time.hours, dep_time.minutes, dep_time.seconds);
-        fscanf(fin, "%d:%d:%d", arr_time.hours, arr_time.minutes, arr_time.seconds);
+        fscanf(fin, "%d:%d:%d ", &dep_time.hours, &dep_time.minutes, &dep_time.seconds);
+        fscanf(fin, "%d:%d:%d", &arr_time.hours, &arr_time.minutes, &arr_time.seconds);
         list = (Flight*)realloc(list, ++count * sizeof(Flight));
         list[count - 1].num = num;
-        list[count - 1].name = (char*)malloc(sizeof(char) * (strlen(tmpName) + 1));
-        list[count - 1].surname = (char*)malloc(sizeof(char) * (strlen(tmpSurname) + 1));
-        strcpy(list[count - 1].name, tmpName);
-        strcpy(list[count - 1].surname, tmpSurname);
+        list[count - 1].dep_airport = (char*)malloc(sizeof(char) * (strlen(dep_airport) + 1));
+        list[count - 1].dep_city = (char*)malloc(sizeof(char) * (strlen(dep_city) + 1));
+        list[count - 1].arr_airport = (char*)malloc(sizeof(char) * (strlen(arr_airport) + 1));
+        list[count - 1].arr_city = (char*)malloc(sizeof(char) * (strlen(arr_city) + 1));
+        strcpy(list[count - 1].dep_airport, dep_airport);
+        strcpy(list[count - 1].dep_city, dep_city);
+        strcpy(list[count - 1].arr_airport, arr_airport);
+        strcpy(list[count - 1].arr_city, arr_city);
+        list[count - 1].dep_time.hours = dep_time.hours;
+        list[count - 1].dep_time.minutes = dep_time.minutes;
+        list[count - 1].dep_time.seconds = dep_time.seconds;
+        list[count - 1].arr_time.hours = arr_time.hours;
+        list[count - 1].arr_time.minutes = arr_time.minutes;
+        list[count - 1].arr_time.seconds = arr_time.seconds;
     }
     fclose(fin);
+
+
+    //TODO: Интерактивный диалог
+    shrink(list,1,&count);
+
+    FILE *fout;
+    if(!(fout = fopen(argv[1], "w")))
+    {
+        perror("File opening error\n");
+        exit(-6);
+    }
+
+    int i;
+    for(i = 0; i < count; i++)
+    {
+        fprintf(fout, "%d %s %s %s %s ", list[i].num, list[i].dep_airport, list[i].dep_city, list[i].arr_airport, list[i].arr_city);
+        fprintf(fout, "%02d:%02d:%02d ", list[i].dep_time.hours, list[i].dep_time.minutes, list[i].dep_time.seconds);
+        fprintf(fout, "%02d:%02d:%02d", list[i].arr_time.hours, list[i].arr_time.minutes, list[i].arr_time.seconds);
+        if(i != count - 1)
+        {
+            fprintf(fout, "\n");
+        }
+        free(list[i].dep_airport);
+        free(list[i].dep_city);
+        free(list[i].arr_airport);
+        free(list[i].arr_city);
+    }
+    free(list);
+
+    fclose(fout);
 }
