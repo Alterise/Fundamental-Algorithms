a0abf6259c335ee3c04b732a21bfb13bf8760035
Alex <alex.tereshkov01@mail.ru>
2020-11-03 12:46:37 +0300

    Create task2_2.c

---
+++ Lab3/task2_2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdarg.h>
+#include <time.h>
+
+int main()
+{
+    int k, l, digit, digit_fin;
+    printf("Input k: ");
+    scanf("%d", &k);
+    printf("Input l: ");
+    scanf("%d", &l);
+    k--;
+    if (l > k)
+    {
+        perror("Wrong input");
+        exit(-1);
+    }
+    
+    digit = 1 << k;
+    digit_fin = 1 << k + 1;
+    
+    int i;
+    for (i = digit; i < digit_fin; i++)
+    {
+        int tmp = i, counter = 0;
+        while (tmp)
+        {
+            if(tmp % 2 == 1) counter++;
+            else 
+            {
+                if (counter == l)
+                {
+                    break;
+                }
+                else counter = 0;
+            }
+            tmp /= 2;
+        }
+        if (counter == l)
+        {
+            printf("%d\n", i);
+        }
+    }
+}
