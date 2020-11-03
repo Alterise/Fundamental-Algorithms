ce9c24e25e037a37e161fd242e6ebcc36ea5ae12
Alex <alex.tereshkov01@mail.ru>
2020-11-04 00:38:31 +0300

    Update task1.c

--- Lab3/task1.c
+++ Lab3/task1.c
@@ -6,6 +6,22 @@
 #include <stdarg.h>
 #include <time.h>
 
+char dec_to_curr(int digit);
+int bit_sum(int a, int b);
+int bit_substr(int a, int b);
+int bit_inverse(int digit);
+
+
+char dec_to_curr(int digit)
+{
+    if (digit < 10)
+    {
+        return bit_sum(digit, '0');
+    }
+    else return bit_sum(bit_substr(digit, 10), 'A');
+    
+}
+
 int bit_sum(int a, int b)
 {
     int buff1, buff2, buff3 = 0, fin = 0;
@@ -75,32 +91,94 @@ int bit_inverse(int digit)
     return digit;
 }
 
-char *change_sys(int digit, int base)
+char *change_sys(int digit, int base, char *fin)
 {
-    char fin[33];
+    int shift = 0;
+    int buff;
     
-    if(!digit) return 0;
-    int fin_digit, length = 1;
+    if(!digit) 
+    {
+        fin[0] = '0';
+        fin[1] = '\0';
+        return fin;
+    }
+    int fin_digit, length = 0, part = 0;
     int k = 1;
+
+    while(k < base)
+    {
+        part = bit_sum(part, 1);
+        k <<= 1;
+    }
+
+    
+    if (digit < 0)
+    {
+        fin[0] = '-';
+        shift = 1;
+        digit = bit_sum(bit_inverse(digit ^ (1 << 31)), 1);
+    }
+    
+    k = 1;
     while (k <= digit)
     {
         length = bit_sum(length, 1);
         k <<= 1;
     }
 
+    int i = 0, j = 0;
 
-    return fin_digit;
+    k = 0;
+    char tmp[33];
+    while (i < length)
+    {
+        if(j < part) 
+        {
+            if((1 << i) & digit) buff = bit_sum(buff, 1 << j);
+        }
+        else
+        {
+            tmp[k] = dec_to_curr(buff);
+            k = bit_sum(k, 1);
+            j = 0;
+            buff = 0;
+            if((1 << i) & digit) buff = bit_sum(buff, 1 << j);
+        }
+        
+        j = bit_sum(j, 1);
+        i = bit_sum(i, 1);
+    }
+    if(buff)
+    {
+        tmp[k] = dec_to_curr(buff);
+        k = bit_sum(k, 1);
+    } 
+    if(shift) k = bit_sum(k, shift);
+    fin[k] = '\0';
+
+    i = shift, j = bit_substr(k, 1);
+    while(i < k)
+    {
+        fin[j] = tmp[bit_substr(i, shift)];
+        j = bit_substr(j, 1);
+        i = bit_sum(i, 1);
+    }
+    
+
+    return fin;
 }
 
 int main()
 {
     int digit;
-    int digit2;
+    char fin[33];
+    // int digit2;
     scanf("%d", &digit);
-    scanf("%d", &digit2);
+    // scanf("%d", &digit2);
+    change_sys(digit, 32, &fin);
+    printf("%s\n", fin);
+
     // printf("%d\n", bit_substr(digit, digit2));
     // printf("%d", bit_sum(digit, digit2));
-    // digit = change_sys(digit, 2);
-    // digit = bit_inverse(digit);
-    // printf("%d", digit);
+    // printf("%d", bit_sum(bit_inverse(digit ^ (1 << 31)), 1));
 }
