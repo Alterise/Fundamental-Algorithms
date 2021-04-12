a0bf062cd84a8b030d45544301462f17d0f6b379
Alex <alex.tereshkov01@mail.ru>
2021-04-12 16:21:05 +0300

    Add some stuff in Lab2.1 (1.0)

--- Lab2.1/Lab2.1/Lab2.1.cpp
+++ Lab2.1/Lab2.1/Lab2.1.cpp
@@ -9,12 +9,19 @@ using namespace std;
 
 int main()
 {
-    //Matrix matr, matr2(5), matr3(7, 5.25);
-    //matr.print();
-    //matr2.print();
-    //matr3.print();
+    Matrix matr, matr2(7, 671), matr3(7, 5.25);
+    matr.print();
+    matr2.print();
+    matr3.print();
+    Matrix matr4(matr3);
+    cout << endl;
+    matr4.print();
+    cout << endl;
+
+    matr4 = matr2 + matr3;
+    matr4.print();
     try {
-        Matrix matr(-3);
+        Matrix matr6(-3);
     } catch (exception& ex) {
         cout << "Exception caught: " << ex.what() << endl;
         return 0;
--- Lab2.1/Lab2.1/Matrix.h
+++ Lab2.1/Lab2.1/Matrix.h
@@ -7,7 +7,10 @@ using namespace std;
 class Matrix
 {
 public:
-	Matrix(size_t size = 0, double default_value = 0) {
+	Matrix(int size = 0, double default_value = 0) {
+		if (size < 0) {
+			throw invalid_argument("Invalid matrix size");
+		}
 		size_value = size;
 		data = new double* [size_value];
 		for (size_t i = 0; i < size_value; i++)
@@ -19,12 +22,61 @@ public:
 			}
 		}
 	}
+
+	Matrix(const Matrix& to_copy) {
+		size_value = to_copy.size_value;
+		data = new double* [size_value];
+		for (size_t i = 0; i < size_value; i++)
+		{
+			data[i] = new double[size_value];
+			for (size_t j = 0; j < size_value; j++)
+			{
+				data[i][j] = to_copy.data[i][j];
+			}
+		}
+	}
+
 	~Matrix() {
 		for (size_t i = 0; i < size_value; i++)
 		{
 			delete data[i];
 		}
 		delete[] data;
+	}
+
+	Matrix operator=(const Matrix& rhs) {
+		for (size_t i = 0; i < size_value; i++)
+		{
+			delete data[i];
+		}
+		delete[] data;
+
+		size_value = rhs.size_value;
+		data = new double* [size_value];
+		for (size_t i = 0; i < size_value; i++)
+		{
+			data[i] = new double[size_value];
+			for (size_t j = 0; j < size_value; j++)
+			{
+				data[i][j] = rhs.data[i][j];
+			}
+		}
+		return *this;
+	}
+
+	Matrix operator+(const Matrix& rhs) {
+		if (size_value != rhs.size_value) {
+			throw invalid_argument("Matrices' sizes aren't equal");
+		}
+
+		for (size_t i = 0; i < size_value; i++)
+		{
+			for (size_t j = 0; j < size_value; j++)
+			{
+				data[i][j] += rhs.data[i][j];
+			}
+		}
+		return *this;
 	}
 
 	void print() {
@@ -37,6 +89,7 @@ public:
 			cout << endl;
 		}
 	}
+
 private:
 	double** data;
 	size_t size_value;
