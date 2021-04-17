02e72d1dc2911a3f08462748c85e8e3f0eea494a
Alex <alex.tereshkov01@mail.ru>
2021-04-17 15:21:25 +0300

    Add some stuff in Lab2.1 (1.1)

--- Lab2.1/Lab2.1/Lab2.1.cpp
+++ Lab2.1/Lab2.1/Lab2.1.cpp
@@ -4,26 +4,45 @@
 #include <iostream>
 #include <exception>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
 int main()
 {
-    Matrix matr, matr2(7, 671), matr3(7, 5.25);
-    matr.print();
-    matr2.print();
-    matr3.print();
-    Matrix matr4(matr3);
-    cout << endl;
-    matr4.print();
-    cout << endl;
+    stringstream my_stream("[[1, 2, 3], [4, 55, 6], [7, 8, 9]]");
+    Matrix matr(7, 10000), matr2(3, 3), matr3(3, 5);
 
-    matr4 = matr2 + matr3;
-    matr4.print();
+    cout << matr2 << endl;
+    cout << matr3 << endl;
+    Matrix matr4 = matr2 * matr3;
+    cout << matr2 << endl;
+    cout << matr4 << endl;
+    matr4 *= 4;
+    cout << 4 * matr4 << endl;
     try {
-        Matrix matr6(-3);
+        my_stream >> matr2;
     } catch (exception& ex) {
         cout << "Exception caught: " << ex.what() << endl;
         return 0;
     }
+    cout << matr2 << endl;
+    cout << transpose(matr2) << endl;
+    cout << determinant(matr2) << endl;
+    cout << (matr2 != matr2) << endl;
+    int a = 1;
+    int b = 1;
+    cout << matr2[{a, b}] << endl;
+    my_stream.clear();
+    my_stream.str("[[2, 3], [55, 6]]");
+    cout << my_stream.str() << endl;
+    Matrix matr5(2);
+    try {
+        my_stream >> matr5;
+    }
+    catch (exception& ex) {
+        cout << "Exception caught: " << ex.what() << endl;
+        return 0;
+    }
+    cout << matr5;
 }
--- Lab2.1/Lab2.1/Matrix.h
+++ Lab2.1/Lab2.1/Matrix.h
@@ -1,11 +1,22 @@
 #pragma once
 #include <exception>
 #include <iostream>
+#include <sstream>
+#include <cmath>
 
 using namespace std;
 
+struct Index
+{
+	int y;
+	int x;
+};
+
 class Matrix
 {
+private:
+	double** data;
+	size_t size_value;
 public:
 	Matrix(int size = 0, double default_value = 0) {
 		if (size < 0) {
@@ -64,7 +75,7 @@ public:
 		return *this;
 	}
 
-	Matrix operator+(const Matrix& rhs) {
+	Matrix operator+=(const Matrix& rhs) {
 		if (size_value != rhs.size_value) {
 			throw invalid_argument("Matrices' sizes aren't equal");
 		}
@@ -79,18 +90,269 @@ public:
 		return *this;
 	}
 
-	void print() {
+	Matrix operator+(const Matrix& rhs) {
+		if (size_value != rhs.size_value) {
+			throw invalid_argument("Matrices' sizes aren't equal");
+		}
+
+		Matrix sum(*this);
+		sum += rhs;
+		return sum;
+	}
+
+	Matrix operator-=(const Matrix& rhs) {
+		if (size_value != rhs.size_value) {
+			throw invalid_argument("Matrices' sizes aren't equal");
+		}
+
 		for (size_t i = 0; i < size_value; i++)
 		{
 			for (size_t j = 0; j < size_value; j++)
 			{
-				cout << data[i][j] << " ";
+				data[i][j] -= rhs.data[i][j];
 			}
-			cout << endl;
 		}
+		return *this;
+	}
+
+	Matrix operator-(const Matrix& rhs) {
+		if (size_value != rhs.size_value) {
+			throw invalid_argument("Matrices' sizes aren't equal");
+		}
+
+		Matrix diff(*this);
+		diff -= rhs;
+		return diff;
+	}
+
+	Matrix operator*=(const Matrix& rhs) {
+		if (size_value != rhs.size_value) {
+			throw invalid_argument("Matrices' sizes aren't equal");
+		}
+
+		Matrix tmp(*this);
+
+		for (size_t i = 0; i < size_value; i++)
+		{
+			for (size_t j = 0; j < size_value; j++)
+			{
+				double sum = 0;
+				for (size_t k = 0; k < size_value; k++)
+				{
+					sum += tmp.data[j][k] * rhs.data[k][j];
+				}
+				data[i][j] = sum;
+			}
+		}
+		return *this;
+	}
+
+	Matrix operator*=(const double& rhs) {
+
+		for (size_t i = 0; i < size_value; i++)
+		{
+			for (size_t j = 0; j < size_value; j++)
+			{
+				data[i][j] *= rhs;
+			}
+		}
+		return *this;
+	}
+
+	Matrix operator*(const Matrix& rhs) {
+		if (size_value != rhs.size_value) {
+			throw invalid_argument("Matrices' sizes aren't equal");
+		}
+
+		Matrix tmp(*this);
+		tmp *= rhs;
+		return tmp;
+	}
+
+	Matrix operator*(const double& rhs) {
+
+		Matrix tmp(*this);
+		tmp *= rhs;
+		return tmp;
+	}
+
+	friend Matrix operator*(const double& lhs, const Matrix& rhs) {
+		Matrix tmp(rhs);
+		tmp *= lhs;
+		return tmp;
+	}
+
+	bool operator==(const Matrix& rhs) {
+		if (size_value != rhs.size_value) {
+			throw invalid_argument("Matrices' sizes aren't equal");
+		}
+
+		double e = 1e-6;
+		for (size_t i = 0; i < size_value; i++)
+		{
+			for (size_t j = 0; j < size_value; j++)
+			{
+				if (fabs(data[i][j] - rhs.data[i][j]) > 1e-6) return false;
+			}
+		}
+		return true;
+	}
+
+	bool operator!=(const Matrix& rhs) {
+		return !(*this == rhs);
+	}
+
+	double& operator[](const Index& index) {
+		if (index.x < 0 || index.y < 0 || index.x >= size_value || index.y >= size_value) {
+			throw invalid_argument("Index out of bounds");
+		}
+
+		return data[index.y][index.x];
+	}
+
+	friend ostream& operator<<(ostream& stream, const Matrix& matrix) {
+		for (size_t i = 0; i < matrix.size_value; i++)
+		{
+			for (size_t j = 0; j < matrix.size_value; j++)
+			{
+				stream << matrix.data[i][j] << " ";
+			}
+			stream << endl;
+		}
+		return stream;
+	}
+
+	friend istream& operator>>(istream& stream, Matrix& matrix) {
+		string str_tmp;
+		getline(stream, str_tmp);
+		istringstream tmp_stream(str_tmp);
+		Matrix tmp = matrix;
+		char sym;
+		tmp_stream >> sym;
+		if (sym != '[') {
+			throw invalid_argument("Wrong input");
+		}
+		for (size_t i = 0; i < tmp.size_value; i++)
+		{
+			tmp_stream >> sym;
+			if (sym != '[') {
+				throw invalid_argument("Wrong input");
+			}
+			for (size_t j = 0; j < tmp.size_value; j++)
+			{
+				try {
+					tmp_stream >> tmp.data[i][j];
+				} catch(exception& ex) {
+					throw invalid_argument("Wrong input");
+				}
+				if ((j + 1) < tmp.size_value) {
+					tmp_stream >> sym;
+					if (sym != ',') {
+						throw invalid_argument("Wrong input");
+					}
+				}
+			}
+			tmp_stream >> sym;
+			if (sym != ']') {
+				throw invalid_argument("Wrong input");
+			}
+			if ((i + 1) < tmp.size_value) {
+				tmp_stream >> sym;
+				if (sym != ',') {
+					throw invalid_argument("Wrong input");
+				}
+			}
+		}
+		if (sym != ']') {
+			throw invalid_argument("Wrong input");
+		}
+		tmp_stream >> sym;
+		while ((sym = tmp_stream.get()) == ' ');
+		if (sym != EOF) {
+			throw invalid_argument("Wrong input");
+		}
+		matrix = tmp;
+		return stream;
+	}
+
+	friend Matrix transpose(const Matrix& matrix) {
+		Matrix new_matrix(matrix);
+		for (size_t i = 0; i < new_matrix.size_value; i++)
+		{
+			for (size_t j = i + 1; j < new_matrix.size_value; j++)
+			{
+				swap(new_matrix.data[i][j], new_matrix.data[j][i]);
+			}
+		}
+		return new_matrix;
+	}
+
+	friend double determinant(const Matrix& matrix) {
+		//Computing determinant using Gaussian elimination
+		double det = 1, mtp, key_elem;
+		Matrix tmp_matrix(matrix);
+		for (size_t i = 0; i < tmp_matrix.size_value; i++)
+		{
+			key_elem = tmp_matrix.data[i][i];
+			int j;
+			if (!key_elem)
+			{
+				j = i + 1;
+				while (!key_elem && j < tmp_matrix.size_value)
+				{
+					key_elem = tmp_matrix.data[j][i];
+					j++;
+				}
+				j--;
+				if (!key_elem)
+				{
+					return 0;
+				}
+				else
+				{
+					for (size_t k = i; k < tmp_matrix.size_value; k++)
+					{
+						tmp_matrix.data[i][k] += tmp_matrix.data[j][k];
+					}
+				}
+			}
+
+			for (j = i + 1; j < tmp_matrix.size_value; j++)
+			{
+				mtp = tmp_matrix.data[j][i] / key_elem;
+				tmp_matrix.data[j][i] = 0;
+				for (size_t k = i + 1; k < tmp_matrix.size_value; k++)
+				{
+					tmp_matrix.data[j][k] = tmp_matrix.data[j][k] - tmp_matrix.data[i][k] * mtp;
+				}
+			}
+			det *= key_elem;
+		}
+
+		return det;
 	}
 
 private:
-	double** data;
-	size_t size_value;
+	Matrix getminor(const Index& index){
+		size_t m_i = 0, m_j = 0;
+		Matrix block(size_value - 1);
+		for (size_t i = 0; i < size_value; i++)
+		{
+			if (i == index.y) {
+				continue;
+			}
+			m_j = 0;
+			for (size_t j = 0; j < size_value; j++)
+			{
+				if (j == index.x) {
+					continue;
+				}
+				block.data[m_i][m_j] = data[i][j];
+				m_j++;
+			}
+			m_i++;
+		}
+		return block;
+	}
 };
+
