df0fa13b91b09dc98b74fd173f78a2d14d5c17bf
Alex <alex.tereshkov01@mail.ru>
2021-04-28 16:02:14 +0300

    Add Lab2.2

---
+++ Lab2.2/BinaryTrees/binary_tree.h
@@ -0,0 +1,15 @@
+#pragma once
+template<typename T>
+class binary_tree
+{
+public:
+	binary_tree(comparator(const &T, const&T))) {
+			
+	}
+	
+	virtual void insert(const T& key) = 0;
+	virtual void remove(const T& key) = 0;
+	virtual binary_tree& search(const T& key) = 0;
+private:
+	comparator<T> _comparator = 
+};
