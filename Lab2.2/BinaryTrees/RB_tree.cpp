e636819914341e2141e31302eb56fb7c36c68cd2
Alex <alex.tereshkov01@mail.ru>
2021-04-30 01:51:05 +0300

    Add some stuff in Lab2.2 (1.1)

---
+++ Lab2.2/BinaryTrees/RB_tree.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "RB_tree.h"
+
+template <typename T> void RB_tree<T>::insert(const int &key) {
+
+}
+template <typename T> RB_tree<T>& RB_tree<T>::search(const int &key) {
+    RB_tree tree(this->_comparator);
+    return tree;
+}
+
+template <typename T> void RB_tree<T>::remove(const int &key) {
+}
