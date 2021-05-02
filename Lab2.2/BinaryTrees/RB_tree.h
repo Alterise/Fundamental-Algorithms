97e1f0c205f1f07b6b2afea5515350548a370980
Alex <alex.tereshkov01@mail.ru>
2021-05-03 01:12:38 +0300

    Add some stuff in Lab2.2 (2.0)

--- Lab2.2/BinaryTrees/RB_tree.h
+++ Lab2.2/BinaryTrees/RB_tree.h
@@ -2,12 +2,45 @@
 #include "binary_tree.h"
 
 template <typename T>
+struct node {
+    T data;
+    node<T> left_child;
+    node<T> right_child;
+    bool is_black;
+};
+
+template <typename T>
 class RB_tree : public binary_tree<T>
 {
 public:
-//    using binary_tree<T>::binary_tree;
-    explicit RB_tree(strategy<T>* comparator) : binary_tree<T>(comparator) {}
-    void insert(const T&) override;
-    void remove(const T&) override;
-    bool search(const T&) override;
+    explicit RB_tree(strategy<T>* comparator) : binary_tree<T>(comparator) {
+        _data = NULL;
+        _left_child = nullptr;
+        _right_child = nullptr;
+        is_black = true;
+        RB
+    }
+    void insert(const T& key) override {
+
+    }
+
+    void remove(const T& key) override {
+
+    }
+
+    bool search(const T& key) override {
+        // TODO: переделать под структуры
+        if((this->_comparator->compare(this->root, key)) == 0) {
+            return true;
+        } else if(((this->_comparator->compare(this->_data, key)) < 0) || this->_left_child != nullptr) {
+            return this->_left_child->search(key);
+        } else if(((this->_comparator->compare(this->_data, key)) < 0) || this->_left_child != nullptr) {
+            return this->_right_child->search(key);
+        } else {
+            return false;
+        }
+    }
+
+private:
+    node<T> _root;
 };
--- Lab2.2/main.cpp
+++ Lab2.2/main.cpp
@@ -1,8 +1,9 @@
-//#include <iostream>
+#include <iostream>
 #include "RB_tree.h"
 #include "concrete_strategies.h"
 
 int main()
 {
     RB_tree<int> tree(new int_strategy);
+    std::cout << tree.get_value();
 }
