9f91c98290805e23e591b48e15f7782bb9dc59f9
Alex <alex.tereshkov01@mail.ru>
2021-05-08 17:13:51 +0300

    Add some stuff in Lab2.2 (4.3) - With duplicates

--- Lab2.2/Strategies/concrete_strategies.h
+++ Lab2.2/Strategies/concrete_strategies.h
@@ -1,41 +1,9 @@
 #pragma once
 #include <string>
+#include <algorithm>
+#include <cctype>
 #include "strategy.h"
-
-struct Date {
-    int day;
-    int month;
-    int year;
-
-    bool operator<(const Date& rhs) const {
-        if(year != rhs.year) {
-            return year < rhs.year;
-        } else if(month != rhs.month) {
-            return month < rhs.month;
-        } else {
-            return day < rhs.day;
-        }
-    }
-    bool operator==(const Date& rhs) const {
-        return ((year == rhs.year) && (month == rhs.month) && (day == rhs.day));
-    }
-    bool operator>(const Date& rhs) const {
-        if(*this == rhs) {
-            return false;
-        } else {
-            return !(*this < rhs);
-        }
-    }
-    bool operator!=(const Date& rhs) const {
-        return !(*this == rhs);
-    }
-};
-
-struct Message {
-    Date date;
-	std::string name;
-	std::string data;
-};
+#include "messages.h"
 
 class message_strategy : public strategy<Message> {
 public:
@@ -56,8 +24,138 @@ public:
     }
 };
 
+class message_strategy_date : public strategy<Message*> {
+public:
+    int compare(Message* const &lhs, Message* const &rhs) override {
+        if(lhs->date.year != rhs->date.year) {
+            return lhs->date.year - rhs->date.year;
+        } else if (lhs->date.month != rhs->date.month) {
+            return lhs->date.month - rhs->date.month;
+        } else if (lhs->date.day != rhs->date.day) {
+            return lhs->date.day - rhs->date.day;
+        } else if (lhs != rhs) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+};
+
+//class message_strategy_username : public strategy<Message> {
+//public:
+//    int compare(const Message& lhs, const Message& rhs) override {
+//        if (lhs.name != rhs.name) {
+//            return lhs.name < rhs.name ? -1 : 1;
+//        } else {
+//            return 0;
+//        }
+//    }
+//};
+//
+//class message_strategy_data : public strategy<Message> {
+//public:
+//    int compare(const Message& lhs, const Message& rhs) override {
+//        std::string lhs_data = lhs.data;
+//        std::string rhs_data = rhs.data;
+//        std::transform(lhs_data.begin(), lhs_data.end(), lhs_data.begin(), tolower);
+//        std::transform(rhs_data.begin(), rhs_data.end(), rhs_data.begin(), tolower);
+//        if (lhs_data != rhs_data) {
+//            return lhs_data < rhs_data ? -1 : 1;
+//        } else {
+//            return 0;
+//        }
+//    }
+//};
+//
+//class message_strategy_username_date : public strategy<Message> {
+//public:
+//    int compare(const Message& lhs, const Message& rhs) override {
+//        std::string lhs_name = lhs.name;
+//        std::string rhs_name = rhs.name;
+//        std::transform(lhs_name.begin(), lhs_name.end(), lhs_name.begin(), tolower);
+//        std::transform(rhs_name.begin(), rhs_name.end(), rhs_name.begin(), tolower);
+//        if (lhs_name != rhs_name) {
+//            return lhs_name < rhs_name ? -1 : 1;
+//        } else if(lhs.date.year != rhs.date.year) {
+//            return lhs.date.year - rhs.date.year;
+//        } else if (lhs.date.month != rhs.date.month) {
+//            return lhs.date.month - rhs.date.month;
+//        } else if (lhs.date.day != rhs.date.day) {
+//            return lhs.date.day - rhs.date.day;
+//        } else {
+//            return 0;
+//        }
+//    }
+//};
+
 class int_strategy : public strategy<int> {
     int compare(const int& lhs, const int& rhs) override {
         return lhs - rhs;
+    }
+};
+
+class int_ptr_strategy : public strategy<int*> {
+    int compare(int* const &lhs, int* const &rhs) override {
+        if ((*lhs - *rhs) == 0) {
+            if (lhs == rhs) {
+                return 0;
+            } else {
+                return 1;
+            }
+        } else {
+            return *lhs - *rhs;
+        }
+    }
+};
+
+class int_strategy_reverse : public strategy<int> {
+    int compare(const int& lhs, const int& rhs) override {
+        return rhs - lhs;
+    }
+};
+
+class int_ptr_strategy_reverse : public strategy<int*> {
+    int compare(int* const &lhs, int* const &rhs) override {
+        if ((*rhs - *lhs) == 0) {
+            if (rhs == lhs) {
+                return 0;
+            } else {
+                return 1;
+            }
+        } else {
+            return *rhs - *lhs;
+        }
+    }
+};
+
+class int_strategy_odd_first : public strategy<int> {
+    int compare(const int& lhs, const int& rhs) override {
+        if ((lhs % 2) == (rhs % 2)) {
+                return lhs - rhs;
+        } else if (lhs % 2) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+};
+
+class int_ptr_strategy_odd_first : public strategy<int*> {
+    int compare(int* const &lhs, int* const &rhs) override {
+        if ((*lhs % 2) == (*rhs % 2)) {
+            if ((*lhs - *rhs) == 0) {
+                if (lhs == rhs) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            } else {
+                return *lhs - *rhs;
+            }
+        } else if (*lhs % 2) {
+            return -1;
+        } else {
+            return 1;
+        }
     }
 };
--- Lab2.2/main.cpp
+++ Lab2.2/main.cpp
@@ -4,30 +4,87 @@
 #include <time.h>
 #include "RB_tree.h"
 #include "concrete_strategies.h"
+#include "relation.h"
+#include "messages.h"
+#include "message_parser.h"
 
-int main()
-{
-    srand(time(0));
-    const int N = 100;
-    RB_tree<int> tree(new int_strategy);
-    std::vector<int> buffer;
-    std::vector<int> buffer2;
-    for (int i = 0; i < N; ++i) {
-        buffer.push_back(rand() % 25000 - 12500);
-        int a = buffer.back();
-        tree.insert(a);
-    }
-    for (const auto &item : buffer) {
-        cout << item << " ";
-    }
-    cout << endl << endl;
-    tree.print();
-    for (int i = 0; i < N; ++i) {
-        int a = (rand() % (N - i));
-        int b = buffer[a];
-        tree.remove(b);
-        buffer.erase(buffer.begin() + a);
-        buffer2.push_back(b);
+const int CURRENT_TEST = 2;
+
+int main() {
+    // RB Tree test
+    if (CURRENT_TEST == 1) {
+        srand(time(0));
+        const int N = 100;
+        RB_tree<int> tree(new int_strategy);
+        std::vector<int> buffer;
+        std::vector<int> buffer2;
+        for (int i = 0; i < N; ++i) {
+            buffer.push_back(rand() % 25000 - 12500);
+            int a = buffer.back();
+            tree.insert(a);
+        }
+        for (const auto &item : buffer) {
+            std::cout << item << " ";
+        }
+        std::cout << std::endl << std::endl;
         tree.print();
+        for (int i = 0; i < N; ++i) {
+            int a = (rand() % (N - i));
+            int b = buffer[a];
+            tree.remove(b);
+            buffer.erase(buffer.begin() + a);
+            buffer2.push_back(b);
+            tree.print();
+        }
     }
+
+    // Class relation test
+    if (CURRENT_TEST == 2) {
+        relation<int> rel(new int_strategy);
+        rel.insert(5);
+        rel.insert_tree(new RB_tree<int*>(new int_ptr_strategy));
+        rel.insert_tree(new RB_tree<int*>(new int_ptr_strategy_reverse));
+        rel.insert(8);
+        rel.insert(11);
+        rel.insert(2);
+        rel.insert(11);
+        int id = rel.insert_tree(new RB_tree<int*>(new int_ptr_strategy_odd_first));
+        rel.insert(15);
+//        auto ptr = rel.search(id, 8);
+//        if (ptr != nullptr) {
+//            std::cout << *ptr << std::endl;
+//        } else {
+//            std::cout << "No" << std::endl;
+//        }
+//        ptr = rel.search(id, 8);
+//        if (ptr != nullptr) {
+//            std::cout << *ptr << std::endl;
+//        } else {
+//            std::cout << "No" << std::endl;
+//        }
+        rel.remove(5);
+        rel.remove(8);
+        rel.remove(11);
+        rel.remove(2);
+        rel.remove(11);
+        rel.remove(15);
+        std::cout << 1;
+    }
+
+    // Message class test
+//    if (CURRENT_TEST == 3) {
+//        std::vector<Message> messages;
+//        messages = parse_messages("input.txt");
+//        std::vector<Message> messages_dates;
+//        messages_dates.reserve(messages.size());
+//        for (const auto &message : messages) {
+//            messages_dates.push_back({message.date,"",""});
+//        }
+//        relation<Message> rel(new message_strategy, false);
+//        int id_by_date = rel.insert_tree(new RB_tree<Message*> (new message_strategy_date, false));
+////        rel.insert_tree(new RB_tree<Message*> (new message_strategy_username, false));
+////        rel.insert_tree(new RB_tree<Message*> (new message_strategy_data, false));
+////        rel.insert_tree(new RB_tree<Message*> (new message_strategy_username_date, false));
+//    }
+
 }
